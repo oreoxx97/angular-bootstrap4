df2da954935d0455e7d2d1657585048702bcae41
LAPTOP-0MN9J4IO\User <zchaiwat.l@pttdigital.com>
2021-05-18 18:00:42 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { MainsComponent } from './mains/mains.component';
+import { ContentComponent } from './content/content.component';
+import { FootersComponent } from './footers/footers.component';
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { VideoComponent } from './video/video.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    MainsComponent,
+    ContentComponent,
+    FootersComponent,
+    SidebarComponent,
+    VideoComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
